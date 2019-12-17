7428500603220a24c6cd1f9b5d6406d7b23c76e9
Aidan Lincoln <aidantlincoln@gmail.com>
2019-12-17 22:50:56 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,7 +13,7 @@ def assembled_aoh
   :JOELLE_VD => {name: "Joelle van Dyne", occupation: "Radio Personality"},
   :PAT_M => {name: "Pat Monteseian", occupation: "Staff"},
   :KATE_G => {name: "Kate Gompert", occupation: "None"},
-  :BRUCE_G =>
+  :BRUCE_G => {name: "Bruce Green", occupation: "Fan of Mildred"}
   ]
 end
 
